a2f17630853d8825f4bdbec27a702456b8d84225
paulcheung22 <6227659@qq.com>
2020-06-18 08:48:47 +0800

    1.对以下网站的视频课进行自动学习，完成：总学时216，其中专业课116.5，一般公需99.5； 专业技术人员学习新干线： https://pro.learning.gov.cn/ 用户名：13957169677 密码：123qweasd$

--- MouseClick.py
+++ MouseClick.py
@@ -1,59 +1,66 @@
 import win32api
 import win32con
 import time
+import traceback
 
 
-def click(postion):
-    win32api.SetCursorPos(postion)
-    time.sleep(1)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-
+def click(position):
+    try:
+        win32api.SetCursorPos(position)
+        time.sleep(0.2)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        print(position)
+    except:
+        traceback.print_exc()
+        
 
 time.sleep(3)
 
+
+page = []
 # 设置鼠标移动到的位置
-# 行表示一个视频从列表选择到播放完毕所有要点击的位置
-# 0：视频列表中第一个视频的位置；1：视频简介中的开始播放按钮位置；2：视频播放器中开始暂停键位置；
-# 3：视频播放器关闭按键位置；4：视频简介关闭按键位置；5：视频列表刷新按钮位置；
-pos = [
-    (885, 418), (887, 637), (77, 432), (1028, 18), (471, 19), (84, 55)
-]
+# 0：视频列表中第一个视频的位置；
+pos_0 = [(880, 374)]
+page.append(pos_0)
+# 1：视频简介中的开始播放按钮位置；
+#pos_1 = [(851, 679), (851, 654), (851, 629), (851, 604), (851, 596), (851, 573), (851, 548)]
+pos_1 = [(851, 654), (851, 629), (851, 604), (851, 596), (851, 573), (851, 548)]
+page.append(pos_1)
+# 2：视频播放器中开始暂停键位置；
+pos_2 = [(846, 150), (62, 546), (62, 546), (39, 413), (39, 413), (76, 637), (76, 637), (428, 132), (428, 132), (500, 491), (500, 491)]
+page.append(pos_2)
+# 3：视频播放器关闭按键位置；
+pos_3 = [(821, 29)]
+page.append(pos_3)
+# 4：视频简介关闭按键位置；
+pos_4 = [(469, 21), (469, 21)]
+page.append(pos_4)
+# 5：视频列表刷新按钮位置；
+pos_5 = [(84, 55)]
+page.append(pos_5)
+
+#设置每个页面点击后，停留的时间；
+sleepTime = []
+for m in range((len(page))):
+    sleepTime.append(1) #先设置默认值
+
+sleepTime[1] = 2 # 1，特殊值；
+sleepTime[2] = 6*60  # 2，特殊值；
+
 
 # 大循环，重复点击视频列表第一个视频的次数
-Count = 10000
+Count = 9999999
 
 for k in range(Count):
-    for i in range(len(pos)):
-        if (i % 6) == 0:  # 0：第一步，移动到视频列表（默认视频列表网页已经打开）；
-            click(pos[i])
-            time.sleep(3)
-        elif (i % 6) == 1: # 1:第二步，单击对应的视简介；
-            click(pos[i])
-            time.sleep(3)
-            click((885, 723))
-            time.sleep(3)
-            click((885, 692))
-            time.sleep(3)
-            click((885, 666))
-            time.sleep(3)
-        elif (i % 6) == 2:  # 2：第三步，由于可能会有网络卡顿，需要有间隔地双击播放器暂停和播放键；
-            n = 0
-            while True:
-                n += 1
-                click(pos[i])
-                click(pos[i])
-                click((112, 581))
-                click((112, 581))
-                time.sleep(10)
+    for i in range(len(page)):
+        posInCurrPage = page[i]
 
-                if n > 60*5/10:
-                    break
-        elif (i % 6) == 3:  # 3：第四步，关闭视频播放网页；
-            click(pos[i])
-            time.sleep(1)
-        elif (i % 6) == 4:  # 4：第五步，关闭视频介绍网页；
-            click(pos[i])
-            time.sleep(1)
-        elif (i % 6) == 5:  # 5：第六步，刷新视频列表页；
-            click(pos[i])
-            time.sleep(1)
+        for j in range(len(posInCurrPage)):
+            pos = posInCurrPage[j]
+            time.sleep(0.5)
+            click(pos)
+
+        time.sleep(sleepTime[i])
+
+    print("Count="+ str(k))
+            
